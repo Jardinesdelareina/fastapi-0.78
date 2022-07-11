7655d35fd0824272aed41d7d2ca4979e92f2d74d
Jardinesdelareina <jardinesdelareina@mail.ru>
2022-07-11 03:25:31 +0300

    field

--- schemas.py
+++ schemas.py
@@ -1,35 +1,44 @@
 from datetime import date
 from typing import List
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, validator, Field
 
 
 class Token(BaseModel):
-    title: str
+    title: str = Field(..., max_length=6)
     amount: float
     price: float
 
-    @validator('amount', 'price')
-    def check_num(cls, num):
-        if num < 0:
-            raise ValueError('Значение не может быть меньше 0')
-        return num
-
 
 class Case(BaseModel):
-    title: str
+    title: str = Field(..., max_length=20)
     compound: List[Token]
-    balance: float
+    balance: float = 0
 
 
 class User(BaseModel):
-    name: str
-    phone: int
-    email: str
+    login: str = Field(
+        ..., 
+        min_length=5, 
+        max_length=15, 
+        description='Длина логина должна быть не меньше 5 и не больше 15 символов'
+        )
+    password: str = Field(
+        ..., 
+        min_length=8, 
+        max_length=20, 
+        description='Длина пароля должна быть не меньше 8 и не больше 15 символов'
+        )
+    phone: int = Field(
+        ..., 
+        lt=14, 
+        description='Не верный формат номера'
+        )
+    email: str = Field(...)
     reg: date
     case: Case
 
-    @validator('phone')
-    def check_phone(cls, num):
-        if len(str(num)) < 11:
-            raise ValueError('Не верный формат номера')
-        return num
+    @validator('email')
+    def check_email(cls, adress):
+        if '@' not in adress:
+            raise ValueError('Не верный формат email')
+        return adress
